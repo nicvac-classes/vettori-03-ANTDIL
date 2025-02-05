a978f0f4299c5576575e00f8a89171cde97ec39f
ANTDIL <antonio.dilernia@marconibari.edu.it>
2025-02-05 23:15:38 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int main() {
+    double n, i, ind;
+
+    cout << "Quanti studenti ci sono in classe?" << endl;
+    cin >> n;
+    string nomi[n];
+    double voti[n];
+    double v;
+
+    i = 0;
+    while (i < n) {
+        cout << "Qual'è il tuo nome?" << endl;
+        cin >> nomi[i];
+        cout << "Qual'è il tuo voto?" << endl;
+        cin >> voti[i];
+        if (i == 0) {
+            v = voti[i];
+        } else {
+            if (v < voti[i]) {
+                v = voti[i];
+            }
+        }
+        i = i + 1;
+    }
+    cout << "I dati della classe sono i seguenti" << endl;
+    i = 0;
+    while (i < n) {
+        if (voti[i] == v) {
+            ind = i;
+        }
+        i = i + 1;
+    }
+    if (ind >= 0) {
+    }
+    cout << "Nome: " << nomi[ind] << " voto: " << voti[ind] << endl;
+    return 0;
+}
